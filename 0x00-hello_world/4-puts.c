011e6d6f9cdc1f94731085865ff18c1b88771d12
m0hamm8d <mm994225@gmail.com>
2023-03-16 17:29:44 -0700

    4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-
+/**
+ *this is simply program to print output
+ *dont use printf
+ */
 int main(void)
 {
   puts("Programming is like building a multilingual puzzle\n");
+
   return (0);
 }

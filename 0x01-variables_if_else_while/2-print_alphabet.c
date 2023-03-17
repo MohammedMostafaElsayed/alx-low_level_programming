810dd5ec2937fb0dc643c6e552c55ea799962868
m0hamm8d <mm994225@gmail.com>
2023-03-17 14:46:05 -0700

    3 task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return Alwayes 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 'a'; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

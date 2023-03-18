3f7a190d5341cdd5a6fdcb933dc72c7d698bc10b
m0hamm8d <mm994225@gmail.com>
2023-03-17 17:34:56 -0700

    9 task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwayes 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '9')
+		{
+			putchar(i);
+		}
+		else
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

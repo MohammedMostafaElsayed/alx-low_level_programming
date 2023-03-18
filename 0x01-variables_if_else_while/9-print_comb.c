d0e59de3d1cc1837e73ecfed7e1cfeb928468fa3
m0hamm8d <mm994225@gmail.com>
2023-03-17 17:57:57 -0700

    9 task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,16 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '9')
+		putchar(i);
+		if (i != '9')
 		{
-			putchar(i);
-		}
-		else
-		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
+		else
+		{
+			putchar('\n');
+		}
 	}
-	putchar('\n');
 	return (0);
 }

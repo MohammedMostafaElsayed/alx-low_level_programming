11269a1ac4630d0dcc8cce8899cefd31f9d8f774
m0hamm8d <mm994225@gmail.com>
2023-03-20 15:19:57 -0700

    100 task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = '0'; i < '9'; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = '1'; j <= '9'; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if (i == '8' && j == '9')
+		   		putchar(i + '0');
+                        	putchar(j + '0');
+			if (i == 8 && j == 9)
 			{
 				continue;
 			}
@@ -23,8 +23,9 @@ int main(void)
 			{
 				putchar(',');
 				putchar(' ');
+			
 			}
-		}
+		}	
 	}
 	putchar('\n');
 	return (0);

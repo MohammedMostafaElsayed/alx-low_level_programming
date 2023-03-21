a3188043138663d1e411a5897e4c0b6e578337a6
m0hamm8d <mm994225@gmail.com>
2023-03-21 10:53:27 -0700

    3 task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - function for check number or char
+ *
+ * Return: 1 (is number) 0 (otherwise)
+ */
+int _islower(int c)
+{
+	int x;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		x = 1;
+	}
+	else
+	{
+		x = 0;
+	}
+	return (x);
+}

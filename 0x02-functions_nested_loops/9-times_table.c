764f8a67b32899be69d4014f3a62680319710a2d
m0hamm8d <mm994225@gmail.com>
2023-03-22 09:48:46 -0700

    9 task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,13 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			printf("%d, ", i * j);
+			if (j != 9)
+			{
+				printf("%3d,", i * j);
+			}else
+			{
+				printf("%3d", i * j);
+			}
 
 		}
 		printf("\n");

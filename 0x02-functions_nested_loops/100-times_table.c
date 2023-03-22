a816f4386f13dbd5c97edfb3862480597f17606a
m0hamm8d <mm994225@gmail.com>
2023-03-22 14:41:11 -0700

    100 task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 
 /**
  * print_times_table - print table
- *@n: is input
+ * @n: is input
  *
  * Return: void
  */
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-	if (n < 15 || n >= 0)
+	if (n < 15 && n > 0)
 	{
 	for (i = 0; i <= n; i++)
 	{

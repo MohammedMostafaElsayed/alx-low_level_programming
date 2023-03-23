ee25d9f4d880757bf7d706a4a771bcff3d57d752
m0hamm8d <mm994225@gmail.com>
2023-03-23 16:21:11 -0700

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - mul two number
+ * @a: first num
+ * @b: secend num
+ *
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}

ddbccc8a24d5755a7b4c81afa50b445c525f18d9
m0hamm8d <mm994225@gmail.com>
2023-03-24 15:19:50 -0700

    9 task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,14 @@ int main(void)
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else if (i == 100)
+			{
+				printf("Buzz");
+			}
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{

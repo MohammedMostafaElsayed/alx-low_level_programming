54012fd314e5e06cad6be948cd0e0fba2e139db6
m0hamm8d <mm994225@gmail.com>
2023-03-27 13:47:49 -0700

    7 task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: is input
+ *
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int j;
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+	}
+	i = i / 2;
+	for (j = i; *(str + j) != '\0'; j++)
+	{
+		_putchar(*(str + j));
+	}
+	_putchar('\n');
+}

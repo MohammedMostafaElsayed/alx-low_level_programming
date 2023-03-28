31ba318e422375e1b2ead9aca9d20893fefc05d5
m0hamm8d <mm994225@gmail.com>
2023-03-27 19:50:46 -0700

    5 task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,19 +4,21 @@
  * rev_string - revesing
  * @s: input
  *
- * Return: no 
+ * Return: no
  */
 void rev_string(char *s)
 {
 	int i;
 	int x;
-	
+	char j;
+
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 	}
-	for (x = i; x >= 0; x--)
+	for (x = 0; x != i / 2; x++)
 	{
-		_putchar(s[x]);
+		j = s[x];
+		s[x] = s[i - x - 1];
+		s[i - x - 1] = j;
 	}
-	_putchar('\n');
 }

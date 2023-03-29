a64e442db91b6fb9b8fbb9ff1d32612e32633a6a
m0hamm8d <mm994225@gmail.com>
2023-03-29 12:31:24 -0700

    1 task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatination string
+ * @dest: first input
+ * @src: second input
+ *
+ * Return: char type
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int x;
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+	}
+	for (x = 0; x < n && *(src + x) != '\0'; x++)
+	{
+		*(dest + i + x) = *(src + x);
+	}
+	*(dest + i + x) = '\0';
+	return (dest);
+}

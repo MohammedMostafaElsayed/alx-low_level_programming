9eaaf1ae9df012c9accf0eea8f6086ff287dd11b
m0hamm8d <mm994225@gmail.com>
2023-03-29 18:55:53 -0700

    3 task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - copmare two str
+ * @s1: first input
+ * @s2: secend input
+ *
+ * Return: int 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	x = *s1 - *s2;
+	return (x);
+}

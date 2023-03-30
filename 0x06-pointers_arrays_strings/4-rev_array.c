ccb3ac70be03a0d95bfadf6b863c6e67ec5ac78f
m0hamm8d <mm994225@gmail.com>
2023-03-29 20:20:34 -0700

    4 task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array
+ * @a: f input
+ * @n: num of element
+ *
+ * Return: none
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		x = *(a + i);
+		*(a + i) = *(a + n -i -1);
+		*(a + n -i -1) = x;
+	}
+}

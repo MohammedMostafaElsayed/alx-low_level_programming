243b0c96767aa3e9d95395c694cd343313544d62
m0hamm8d <mm994225@gmail.com>
2023-03-29 19:35:12 -0700

    3 task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,21 @@
  * @s1: first input
  * @s2: secend input
  *
- * Return: int 
+ * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
 	int x;
+	int i;
 
-	x = *s1 - *s2;
+	x = 0;
+	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
+	{
+		if (*(s1 + i) != *(s2 + i))
+		{
+			x = *(s1 + i) - *(s2 + i);
+		}
+		break;
+	}
 	return (x);
 }

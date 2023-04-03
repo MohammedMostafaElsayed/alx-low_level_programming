e5c0860e31800ca927710dcefbf7c1acf75629e3
m0hamm8d <mm994225@gmail.com>
2023-04-03 07:50:37 -0700

    0 task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - put spacific char in memory
+ * @s: first input
+ * @b: secend input
+ * @n: thid input
+ *
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}

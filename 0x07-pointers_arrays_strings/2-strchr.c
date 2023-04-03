5e47b5b090677a6107546911b9928c42a9410a71
m0hamm8d <mm994225@gmail.com>
2023-04-03 13:02:58 -0700

    2 task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchar - locates char in str
+ * @s: first input
+ * @c: secend input
+ *
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}	
+	return (0);
+}

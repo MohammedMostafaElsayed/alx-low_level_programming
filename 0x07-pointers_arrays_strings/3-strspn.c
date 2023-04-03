72816f89421e07cac44927567f7bc0f44fc84880
m0hamm8d <mm994225@gmail.com>
2023-04-03 16:32:54 -0700

    3 task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - for the string
+ * @s: first input
+ * @accept: secend input
+ *
+ * Return: integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int x;
+
+	i = 0;
+	x = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) == *accept)
+		{
+			x = i + 1;
+			break;
+		}
+		i++;
+	}
+	return (x);
+}

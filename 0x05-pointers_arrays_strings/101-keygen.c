3cea055a9439158837b9bd99fe3830f15e73e9ac
m0hamm8d <mm994225@gmail.com>
2023-04-03 22:44:37 -0700

    task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: zer0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	srand(time(NULL));
+	while (i < 2400)
+	{
+		c = rand % 128;
+		i += c;
+		putchar(c);
+	}
+	putchar(2770 - i);
+	return (0);
+}

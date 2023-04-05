142356527b5c6392129d6a29655c35ef0d02a5ef
m0hamm8d <mm994225@gmail.com>
2023-04-05 00:01:59 -0700

    3 task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - factoril of n
+ * @n: the inpur integer
+ *
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+		return (1);
+}

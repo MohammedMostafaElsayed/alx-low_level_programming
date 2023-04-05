a7a933a7bf3ea3b3a410cf0566083c16bddd5f90
m0hamm8d <mm994225@gmail.com>
2023-04-04 22:22:38 -0700

    0 task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);	
+}

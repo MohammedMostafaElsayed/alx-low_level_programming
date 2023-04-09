360bcef7a85dc9288737921fe7aef71ba0f802ab
m0hamm8d <mm994225@gmail.com>
2023-04-08 20:12:59 -0700

    3 task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: 1
+ * @argv: 2
+ *
+ * Return: none
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 1;
+	if (argc > 1)
+	{
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	}
+	return (0);
+}

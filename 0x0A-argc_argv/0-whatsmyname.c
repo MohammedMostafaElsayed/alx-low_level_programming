b52e139149be94d843955ca0a522f1949ab94be5
m0hamm8d <mm994225@gmail.com>
2023-04-08 17:13:06 -0700

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry poin
+ * @argc: first input
+ * @argv: secend input
+ *
+ * Return: none
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}

e7cdb43131cb64f8a62c4bceca93c361e2b46c84
m0hamm8d <mm994225@gmail.com>
2023-04-08 17:37:22 -0700

    1 task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: 1 input
+ * @argv: 2 input
+ *
+ * Return: none
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}

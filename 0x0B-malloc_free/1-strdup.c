c5e1f4681a3b7722eca0aa1c3db23033efe3d56e
m0hamm8d <mm994225@gmail.com>
2023-04-11 01:22:13 -0700

    1 task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - array
+ * @str: string
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *x;
+	unsigned int i;
+	unsigned int size;
+
+	size = strlen(str);
+	if (*str != '\0')
+	{
+		x = malloc(size * sizeof(char));
+		if (x == NULL)
+		{
+			printf("faild to allocate memory");
+		}
+		else
+		{
+			for (i = 0; *(str + i); i++)
+			{
+				*(x + i) = *(str + i);
+			}
+		}
+	}
+	else
+	{
+		x = '\0';
+	}
+	return (x);
+}

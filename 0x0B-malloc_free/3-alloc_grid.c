17547d0e02e60d816edb7fd3001b63cb5bf4f34c
m0hamm8d <mm994225@gmail.com>
2023-04-12 14:03:17 -0700

    3 task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,20 +17,19 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	x = malloc(width * sizeof(int));
+	x = malloc(height * sizeof(int *));
 	if (x == NULL)
 	{
 		printf("there is no space");
+		return (NULL);
 	}
-	else
-	{
 	for (i = 0; i < height; i++)
 	{
 		x[i] = malloc(width * sizeof(int));
 		if (x[i] == NULL)
 		{
 			printf("there is no space");
-			break;
+			return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
@@ -39,7 +38,6 @@ int **alloc_grid(int width, int height)
 		{
 			x[i][j] = 0;
 		}
-	}
 	}
 	return x;
 }

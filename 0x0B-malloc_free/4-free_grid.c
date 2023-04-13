e1d39ac482f1e45da6e3d678532c7788f3471c0d
m0hamm8d <mm994225@gmail.com>
2023-04-12 20:18:02 -0700

    4 task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * free_grid - free function
+ * @grid: 1 input
+ * @height: 2 input
+ *
+ * Return - void
  */
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

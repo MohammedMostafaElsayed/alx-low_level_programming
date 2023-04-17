620c2a6cb2cd33e5886ed2de62670caa8d0ecd02
m0hamm8d <mm994225@gmail.com>
2023-04-17 10:39:31 -0700

    5 task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free
+ * @d: ptr
+ *
+ * Return: none
+ */
+void free_dog(dog_t *d)
+{
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+	
+}

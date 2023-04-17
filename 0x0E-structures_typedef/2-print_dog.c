b525893abde6a452c33cf6079e4ac7f4cd51d0a9
m0hamm8d <mm994225@gmail.com>
2023-04-17 06:59:47 -0700

    2 task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print element
+ * @d: pointer
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->age == 0)
+	{
+		printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, "(nil)", d->owner);
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
+

e0785361d64047b93276a503303be9d95a4d2ad3
m0hamm8d <mm994225@gmail.com>
2023-04-18 23:18:35 -0700

    0 task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - this fun print name
+ * @name: 1mem
+ * @f: secend mem
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

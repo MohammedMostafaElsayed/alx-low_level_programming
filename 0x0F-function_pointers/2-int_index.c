1747334711e8f56e063b4d56afeed30044bbb6ef
m0hamm8d <mm994225@gmail.com>
2023-04-19 16:42:44 -0700

    2 task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index of array
+ * @array: 1 mem
+ * @size: 2 mem
+ * @cmp: 3 mem
+ *
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
+
+

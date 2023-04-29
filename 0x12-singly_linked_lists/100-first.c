a2eeb4e27fac73d8b258a82a6b578d64e3d6c75a
m0hamm8d <mm994225@gmail.com>
2023-04-28 18:45:00 -0700

    100 task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * bef_main - this function will excute before main
+ *
+ * Return: non
+ */
+void bef_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

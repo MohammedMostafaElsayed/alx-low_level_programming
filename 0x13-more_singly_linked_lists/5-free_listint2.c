7f35ceb4cfaada5d882caa837e5d81f5e9dcc6f8
m0hamm8d <mm994225@gmail.com>
2023-04-30 01:42:49 -0700

    5 task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: first
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *x;
+
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		x = *head;
+		*head = (*head)->next;
+		free(x);
+	}
+}

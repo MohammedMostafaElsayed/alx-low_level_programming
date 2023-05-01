580d44d6b618a0a4313feedb9b80e57d8f1660e7
m0hamm8d <mm994225@gmail.com>
2023-05-01 06:15:47 -0700

    6 task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,14 @@
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *x;
 	int n;
-
+	
 	if (*head == NULL)
 		return (0);
-	n = (*head)->n;
-	(*head) = (*head)->next;
+	x = *head;
+	n = x->n;
+	*head = x->next;
+	free(x);
 	return (n);
 }

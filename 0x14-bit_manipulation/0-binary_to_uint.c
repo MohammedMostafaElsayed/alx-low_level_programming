81bbd91dd871f1acf6e968035ed6a53538b5c763
m0hamm8d <mm994225@gmail.com>
2023-05-04 06:01:40 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,17 +5,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x;
-	int i, len;
+	int i, len, mul;
 
 	if (b == NULL)
 		return (0);
 	len = strlen(b);
 	x = *(b + len - 1) - 48;
+	mul = 1;
 	for (i = 1; i < len; i++)
 	{
+		mul *= 2;
 		if (*(b + len - i - 1) == 49)
 		{
-			x += pow(2, i);
+			x += mul;
 		}
 		else if (*(b + len - i - 1) > 49)
 		{

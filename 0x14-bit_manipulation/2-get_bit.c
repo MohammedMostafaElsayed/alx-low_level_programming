9466e5e3a14dd6346729f6a664f26675ad65c2ff
m0hamm8d <mm994225@gmail.com>
2023-05-05 09:17:32 -0700

    2 task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!n)
-		return (-1);
 	if (((n >> index) & 1) == 1)
 		return (1);
 	else if (((n >> index) & 1) == 0)
 		return (0);
-	return (0);
+	return (-1);
 }

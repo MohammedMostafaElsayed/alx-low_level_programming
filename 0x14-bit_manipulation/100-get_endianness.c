8d3cbc6067af363e4379b08b0525d32cfd585691
m0hamm8d <mm994225@gmail.com>
2023-05-05 23:40:32 -0700

    100 task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - bit order
+ *
+ * Return: int
+ */
+int get_endianness(void)
+{
+	int x = 0x1234;
+	char* y;
+
+	y = (char *)&x;
+	if (*y == 0x12)
+		return (0);
+	else if (*y == 0x34)
+		return (1);
+	return (0);
+}

998a9197ed383e75a650614df0f71cbf75f6f30b
m0hamm8d <mm994225@gmail.com>
2023-05-09 07:20:30 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,10 +41,10 @@ int main(int argc, char **argv)
 	}
 	e1 = close(x);
 	e2 = close(y);
-	if (e1 == -1 || e2 == -1)
+	/**if (e1 == -1 || e2 == -1)
 	{
 		printf("Error: Can't close fd FD_VALUE\n");
 		exit(100);
-	}
+	}*/
 	return (0);
 }

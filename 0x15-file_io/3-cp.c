ca956744bf8b11be3607ce11d8965e2cd60a760a
m0hamm8d <mm994225@gmail.com>
2023-05-09 07:18:39 -0700

    3 task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,25 +9,25 @@
  */
 int main(int argc, char **argv)
 {
-	int x, y;
+	int x, y, e1, e2;
 	int r, w;
 	char rr[1024];
 
 	if (argc != 3)
 	{
-		dprintf("Usage: cp file_from file_to\n");
+		printf("Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	x = open(argv[1], O_RDONLY);
 	if (x < 0)
 	{
-		dprintf("Error: Can't read from file NAME_OF_THE_FILE\n");
+		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
 	y = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (y < 0)
 	{
-		dprintf("Error: Can't write to NAME_OF_THE_FILE\n");
+		printf("Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
 	while ((r = read(x, rr, sizeof(rr))) > 0)
@@ -35,13 +35,15 @@ int main(int argc, char **argv)
 		w = write(y, rr, r);
 		if (w != r)
 		{
-			dprintf("Error: Can't write to NAME_OF_THE_FILE\n");
+			printf("Error: Can't write to NAME_OF_THE_FILE\n");
 			exit(99);
 		}
 	}
-	if (close(x) == -1 || close(y) == -1)
+	e1 = close(x);
+	e2 = close(y);
+	if (e1 == -1 || e2 == -1)
 	{
-		dprintf("Error: Can't close fd FD_VALUE\n");
+		printf("Error: Can't close fd FD_VALUE\n");
 		exit(100);
 	}
 	return (0);

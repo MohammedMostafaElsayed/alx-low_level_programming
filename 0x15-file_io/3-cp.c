1bcbd962cd2f1c31e86e2a1647398262e2a239fa
m0hamm8d <mm994225@gmail.com>
2023-05-09 06:56:55 -0700

    3 task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: first argument
+ * @argv: seconed argument
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+	int r, w;
+	char rr[1024];
+
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	x = open(argv[1], O_RDONLY);
+	if (x < 0)
+	{
+		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	y = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (y < 0)
+	{
+		printf("Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	while ((r = read(x, rr, sizeof(rr))) > 0)
+	{
+		w = write(y, rr, r);
+		if (w != r)
+		{
+			printf("Error: Can't write to NAME_OF_THE_FILE\n");
+			exit(99);
+		}
+	}
+	if (close(x) == -1 || close(y) == -1)
+	{
+		printf("Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	return (0);
+}

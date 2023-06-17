16340b233faef0028864c33e2d2dd60e0db9a117
mohammed <mm994225@gmail.com>
2023-06-17 05:33:48 +0300

    7 task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ unsigned int x;
 x = 0;
 temp = *h;
 if (!h)
-    return NULL;
+return (NULL);
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
@@ -22,20 +22,20 @@ new->n = n;
 new->next = NULL;
 new->prev = NULL;
 if (idx == 0)
-    return (add_dnodeint(&(*h), n));
+return (add_dnodeint(&(*h), n));
 if (idx != 0)
 {
 while (temp != NULL && x < idx - 1)
 {
-    temp = temp->next;
-    x++;
-    if (temp == NULL)
-        return NULL;
+temp = temp->next;
+x++;
+if (temp == NULL)
+return (NULL);
 }
 }
 temp2 = temp->next;
 if (temp2 == NULL)
-    return (add_dnodeint_end(&(*h), n));
+return (add_dnodeint_end(&(*h), n));
 temp->next = new;
 temp2->prev = new;
 new->next = temp2;
